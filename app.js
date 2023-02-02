78cd454a89e2f14cf7601443327fd3395c11c01a
Tone Lopes Monte <119428578+ToneLopes@users.noreply.github.com>
2023-02-02 15:21:22 -0300

    finished

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const citySearchBar = document.getElementById("city-seach-input")
+
+
+// Interação
+const citySearchInput = document.getElementById("city-search-input")
 const citySearchButton = document.getElementById("city-search-button")
+
+//Exibição
 const currentDate = document.getElementById("current-date")
 const cityName = document.getElementById("city-name")
 const weatherIcon = document.getElementById("weather-icon")
@@ -10,3 +15,83 @@ const feelsLikeTemperature = document.getElementById("feels-like-temperature")
 const currentHumidity = document.getElementById("current-humidity")
 const sunriseTime = document.getElementById("sunrise-time")
 const sunsetTime = document.getElementById("sunset-time")
+
+const api_key = "1363ab801829b1767a618632317b13b8"
+
+citySearchButton.addEventListener("click", () => {
+   let cityName = citySearchInput.value
+   getCityWeather(cityName)
+})
+
+navigator.geolocation.getCurrentPosition(
+   (position) => {
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+
+      getCurrentLocationWeather(lat, lon)
+
+   },
+   (err) => {
+      if(err.code === 1){
+      alert("Geolocalização negada pelo usuário, busque manualmente por uma cidade através da barra de pesquisa.")
+   } else 
+     { console.log(err)
+   }
+}
+)
+
+function getCurrentLocationWeather( lat, lon) {
+   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}units=metric&lang=pt_br&appid=${api_key}`)
+   .then((response) => response.json())
+   .then((data) => displayWeather(data))
+   
+}
+
+//https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${api_key}
+
+function getCityWeather(cityName) {
+   
+   weatherIcon.src = `./assets/SVG/loading-icon.svg`
+  
+   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`)
+   .then((response) => response.json())
+   .then((data) => displayWeather(data))
+} 
+
+function displayWeather(data) {
+   let {
+      dt,
+      name,
+      weather: [{ description, icon}],
+      main: { temp, feels_like, humidity },
+      wind: { speed },
+      sys: { sunrise, sunset},
+   } = data
+
+currentDate.textContent = formatDate(dt)
+cityName.textContent = name
+
+weatherIcon.src = `./assets/SVG/${icon}.svg`
+
+weatherDescription.textContent = description;
+currentTemperature.textContent = `${Math.round(temp)}ºC`
+windSpeed.textContent = `${Math.round(speed * 3.6)}km`
+feelsLikeTemperature.textContent = `${Math.round(feels_like)}ºC`
+currentHumidity.textContent = `${humidity}%`
+sunriseTime.textContent = formatTime(sunrise)
+sunsetTime.textContent = formatTime(sunset)
+}
+
+
+function formatDate(epochTime) {
+   let date = new Date(epochTime * 1000)
+   let formattedDate = date.toLocaleDateString('pt-BR', { month: "long", day: 'numeric', weekday: 'long'})
+   return formattedDate
+}
+
+function formatTime(epochTime) {
+   let date = new Date(epochTime * 1000)
+   let hours = date.getHours()
+   let minutes = date.getMinutes()
+   return `${hours}:${minutes}`
+}
